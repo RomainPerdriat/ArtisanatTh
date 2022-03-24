b7c59742860cd29e93ca325891953795c080ab3d
RomainPerdriat <romainperdriat@gmail.com>
2022-03-24 15:06:23 +0100

    feat: add moving card to reparationPage

--- src/components/Reparations/Reparations.jsx
+++ src/components/Reparations/Reparations.jsx
@@ -3,14 +3,37 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './reparations.scss';
 import { Box } from '@material-ui/core';
-
+import BeforeAfter from '../BeforeAfter/BeforeAfter';
+import { Typography } from '@material-ui/core';
 const Reparations = ({className, ...rest}) => {
    return (
+
+
+    
        <Box
             className={classnames('reparations', className)}
             {...rest}
          >
-            REPARATIONS PAGE
+            <Box className = "reparations-container">
+                <Typography>
+                Il y a toujours une solution pour redonner vie à vos objets!
+                </Typography>
+                <Box className ="before-after-list">
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+                <BeforeAfter/>
+               
+            </Box>
+            </Box>
+            
+            
+          
         </Box>
    );
 };
