cbf625e3761cbfdfa5fb8a269d76d689d02f2e9f
RomainPerdriat <romainperdriat@gmail.com>
2022-03-28 14:14:59 +0200

    feat: try another way to filter categories

--- src/components/Creations/Creations.jsx
+++ src/components/Creations/Creations.jsx
@@ -8,20 +8,59 @@ import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { MenuItem } from '@material-ui/core';
-import BeforeAfter from '../BeforeAfter/BeforeAfter'
+import BeforeAfter from '../BeforeAfter/BeforeAfter';
+import { useState } from 'react';
 
 const Creations = ({className, ...rest}) => {
-    const handleChange = async (event) => {
-        console.log(`JE FILTRE`)
+    // const handleChange = async (event) => {
+    //     console.log(`JE FILTRE`)
+    // }
+    const [allBtn, setAllBtn] = useState(true);
+    const [bagBtn, setBagBtn] = useState(false);
+    const [beltBtn, setBeltBtn] = useState(false);
+    const [variousBtn, setVariousBtn] = useState(false);
+ 
+    function handleAllBtnClick () {
+        setAllBtn(!allBtn)
+        setBagBtn(false)
+        setBeltBtn(false)
+        setVariousBtn(false)
     }
+    function handleBagBtnClick () {
+        setBagBtn(!bagBtn)
+        setAllBtn(false)
+        setBeltBtn(false)
+        setVariousBtn(false)
+    }
+    function handleBeltBtnClick () {
+        setBeltBtn(!beltBtn)
+        setAllBtn(false)
+        setBagBtn(false)
+        setVariousBtn(false)
+    }
+    function handleVariousBtnClick () {
+        setVariousBtn(!variousBtn)
+        setAllBtn(false)
+        setBeltBtn(false)
+        setBagBtn(false)
+    }
+    
+
    return (
        <Box
             className={classnames('creations', className)}
             {...rest}
          >
+         
           
             <Box className = "creations-container">
-            <FormControl className="select-form" fullWidth>
+            <Box className = "creations-btns">
+                <button onClick = {handleAllBtnClick} className="btn">Tout</button>
+                <button onClick = {handleBagBtnClick} className="btn">Sacs</button>
+                <button onClick = {handleBeltBtnClick} className="btn">Ceintures</button>
+                <button onClick = {handleVariousBtnClick} className="btn">Divers</button>
+         </Box>   
+            {/* <FormControl className="select-form" fullWidth>
                     <InputLabel id="demo-simple-select-label">Catégories Principales</InputLabel>
                     <Select
                         className="select-field"
@@ -34,16 +73,48 @@ const Creations = ({className, ...rest}) => {
                         <MenuItem value={10}>Sacs</MenuItem>
                         <MenuItem value={20}>Ceinture</MenuItem>
                         <MenuItem value={30}>Autre</MenuItem>
-                            {/* Here i'll need .map to filter all articles */}
                     </Select>
-                </FormControl>
-                <Box className ="creations-list">
-                <BeforeAfter/>
-                <BeforeAfter/>
-                <BeforeAfter/>
-                <BeforeAfter/>
-                
-            </Box>
+                </FormControl> */}
+                {allBtn  &&
+                <>
+                    <Typography>TOUT</Typography>
+                    <Box className ="creations-list">
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    </Box>
+                </>}
+                {bagBtn &&
+                <>
+                    <Typography>Sacs</Typography>
+                    <Box className ="creations-list">
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    </Box>
+                </>}
+                {beltBtn &&
+                <>
+                    <Typography>Ceintures</Typography>
+                    <Box className ="creations-list">
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    </Box>
+                </>}
+                {variousBtn &&
+                <>
+                    <Typography>Divers</Typography>
+                    <Box className ="creations-list">
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    <BeforeAfter/>
+                    </Box>
+                </>}
             </Box>
         
         </Box>
